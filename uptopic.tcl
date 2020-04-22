8fc1e30284d723cf450dfb508145d435007d8617
Hasbullah Masby <63797095+akoeabi@users.noreply.github.com>
2020-04-22 19:46:55 +0800

    Update uptopic.tcl

--- uptopic.tcl
+++ uptopic.tcl
@@ -1,31 +1,31 @@
-#######################################################################
-# 
-#UpTopic.tcl
+################################################## #####################
 #
-#La un interval de timp schimba topicu cu uptimu server-ului unde este #hostat eggdrop-ul.
-#Pentru activare .chanset #canal +uptopic (in DCC)
+# UpTopic.tcl
 #
-#                                       BLaCkShaDoW ProductionS
-#######################################################################
+#At a time change the topic with the server uptime where the #hostat eggdrop is.
+#For activation .chanset #channel +uptopic (in DCC)
+#
+# BLaCkShaDoW ProductionS
+################################################## #####################
 
-#Aici setezi cum vrei sa fie topicu setat
+# Here's how you want it to be topically set
 
-set topic_up "Server UP - Ceas : %time% , Uptime : %days% zile , %hour% ore"
+set topic_up "Server UP - Clock:% time%, Uptime:% days% days,% hour% hours"
 
 
-#Aici setezi dupa cat timp sa reimprospateze topicu (minute)
+#Here you set how long to refresh the topic (minutes)
 
 set change_time "10"
 
-#Aici setezi offtopic (topic care va fii pus in caz ca uptimu nu se #poate executa
+# Here you set offtopic (topic that will be put in case the uptimo cannot be executed
 
-set of_topic "Uptime nu este disponibil momentan"
+set of_topic "Uptime is currently unavailable"
 
 
-#######################################################################
+################################################## #####################
 setudef flag uptopic
-if {![info exists topicchange_running]} {
-timer $change_time topicchange
+if {! [info exists topicchange_running]} {
+timer $ change_time topicchange
 set topicchange_running 1
 }
 
@@ -33,23 +33,23 @@ set topicchange_running 1
 proc topicchange {} {
 global change_time topic_up of_topic
 catch {exec uptime} shelluptime
-set text [split $shelluptime];#
-set time [lindex $text 0]
-set days [lindex $text 3]
-set hour [lindex $text 6]
-set hours [string trim $hour ","]
-set replace(%time%) $time
-set replace(%days%) $days
-set replace(%hour%) $hours
-set topics [string map [array get replace] $topic_up]
+set text [split $ shelluptime]; #
+set time [lindex $ text 0]
+set days [lindex $ text 3]
+set hour [lindex $ text 6]
+set hours [string trim $ hour ","]
+set replace (% time%) $ time
+set replace (% days%) $ days
+set replace (% hour%) $ hours
+set topics [string map [array get replace] $ topic_up]
 foreach chan [channels] {
-if {[channel get $chan uptopic]} {
-if {$shelluptime == ""} { putquick "TOPIC $chan :$of_topic" }
-putquick "TOPIC $chan :Getting new UPTIME.."
-puthelp "TOPIC $chan :$topics"
+if {[channel get $ chan uptopic]} {
+if {$ shelluptime == ""} {putquick "TOPIC $ chan: $ of_topic"}
+putquick "TOPIC $ chan: Getting new UPTIME .."
+puthelp "TOPIC $ chan: $ topics"
 }
 }
-timer $change_time topicchange
+timer $ change_time topicchange
 return 1
 }
 
